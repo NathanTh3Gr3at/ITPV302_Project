8dbe7dbf6dc3355781cdf9c7a4f8bf4fc451c422
Wvrmhat <maxnaidoo888@gmail.com>
2024-10-03 02:54:23 +0200

    Added profile view base

--- ThymeToCook/thyme_to_cook/lib/views/profile_screen/profile_view.dart
+++ ThymeToCook/thyme_to_cook/lib/views/profile_screen/profile_view.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:thyme_to_cook/enums/menu_action.dart';
@@ -75,7 +77,61 @@ class ProfileView extends StatelessWidget {
           )
         ],
       ),
+      body: _profileSection(),
       bottomNavigationBar: const BottomNavBar(),
+    );
+  }
+
+  Container _profileSection() {
+    return Container(
+      color: const Color.fromARGB(255, 236, 236, 236),
+      height: 150,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 80,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        //user profile image
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image:
+                              AssetImage("assets/images/placeholder_image.jpg"),
+                        ),
+                      ),
+                    ),
+
+                    // spacing edit profile button
+                    SizedBox(
+                      height: 30,
+                      child: TextButton(
+                        onPressed: () {},
+                        // styles the button
+                        style: TextButton.styleFrom(
+                            side: const BorderSide(color: Colors.black),
+                            backgroundColor: const Color.fromARGB(181, 255, 255, 255),),
+                        child: const Text('Edit profile',
+                        style: TextStyle(color: Colors.black),),
+                      ),
+                    ),
+                  ],
+                ),
+                // section for user details, must fill in
+                const Text("User"),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
