0020578d3ea672949934caf60d3ec7f914a9a906
Wvrmhat <maxnaidoo888@gmail.com>
2024-10-04 03:01:52 +0200

    Updated profile_view

--- ThymeToCook/thyme_to_cook/lib/views/profile_screen/profile_view.dart
+++ ThymeToCook/thyme_to_cook/lib/views/profile_screen/profile_view.dart
@@ -9,9 +9,12 @@ import 'package:thyme_to_cook/services/auth/bloc/auth_event.dart';
 import 'package:thyme_to_cook/utilities/dialogs/logout_dialog.dart';
 import 'package:thyme_to_cook/views/settings_screen/settings_view.dart';
 
+
 class ProfileView extends StatelessWidget {
   static const routeName = '/profile';
   const ProfileView({super.key});
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -77,61 +80,124 @@ class ProfileView extends StatelessWidget {
           )
         ],
       ),
-      body: _profileSection(),
+      body: _profilePage(),
       bottomNavigationBar: const BottomNavBar(),
     );
   }
 
-  Container _profileSection() {
-    return Container(
-      color: const Color.fromARGB(255, 236, 236, 236),
-      height: 150,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 20),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 100,
-                      width: 80,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        //user profile image
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image:
-                              AssetImage("assets/images/placeholder_image.jpg"),
+  Column _profilePage() {
+    return Column(
+      children: [
+        _profileSection(),
+        const SizedBox(
+          height: 20,
+        ), // distance from searchbar
+        _userActivity(),
+        const SizedBox(
+          height: 40,
+        ),
+       ] //
+    );
+  }
+
+  Column _profileSection() {
+    return Column(
+      
+      children: [
+        Padding(
+          
+          padding: const EdgeInsets.only(left: 20,),
+          child: Column(
+            
+            children: [
+              Row(
+                
+                children: [
+                  Column(
+                    
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      
+                      Container(
+                        
+                        height: 100,
+                        width: 80,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          //user profile image
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image:
+                                AssetImage("assets/images/placeholder_image.jpg"),
+                          ),
                         ),
                       ),
-                    ),
-
-                    // spacing edit profile button
-                    SizedBox(
-                      height: 30,
-                      child: TextButton(
-                        onPressed: () {},
-                        // styles the button
-                        style: TextButton.styleFrom(
-                            side: const BorderSide(color: Colors.black),
-                            backgroundColor: const Color.fromARGB(181, 255, 255, 255),),
-                        child: const Text('Edit profile',
-                        style: TextStyle(color: Colors.black),),
+        
+                      // spacing edit profile button
+                      SizedBox(
+                        height: 30,
+                        child: TextButton(
+                          onPressed: () {},
+                          // styles the button
+                          style: TextButton.styleFrom(
+                              side: const BorderSide(color: Colors.black),
+                              backgroundColor: const Color.fromARGB(181, 255, 255, 255),),
+                          child: const Text('Edit profile',
+                          style: TextStyle(color: Colors.black),),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                // section for user details, must fill in
-                const Text("User"),
-              ],
-            ),
-          ],
+                    ],
+                  ),
+                  // section for user details, must fill in
+                  const Text("User"),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
+      ],
+      
+    );
+    
+  }
+  Column _userActivity() {
+    return const Column(
+      children: [
+        Column(
+          
+          children: [
+            DefaultTabController(
+              length: 2, 
+              child: Column(
+                children: [
+                  // tab sections
+                  TabBar(
+                    tabs: [
+                      Tab(child: Text("Kitchen Activitiy"),),
+                      Tab(child: Text("Recipes Created"),),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 300,
+                    child: TabBarView(
+                      // text under each tab
+                      children: [
+                        Card(
+                          // color: Color.fromARGB(255, 252, 253, 242),
+                          // elevation: 5,
+                        child: Center(child: Text("Kitchen Activity"),),
+                        ),
+                        Card(
+                          child: Center(child: Text("Recipes Created"),),
+                        ),
+                      ]),
+                  )
+                ],
+              ))
+          ],
+        )
+      ],
     );
   }
 }
